8128cb7b14b566e1bd2159b59ea56bb76cc1d605
Mitch Steimel <mitch_steimel@ryder.com>
2024-11-26 14:42:51 -0600

    clean up on config files

--- packages/web-components/src/styles/global.ts
+++
@@ -1,9 +0,0 @@
-import { css } from "lit";
-import "../../../tokens/build/css/variables.css";
-
-export const globalStyles = css`{
-  body {
-    font-family: "Nunito Sans", "Helvetica Neue", Helvetica, Arial, sans-serif;
-    color: var(--color-text-primary);
-  }
-}`;
